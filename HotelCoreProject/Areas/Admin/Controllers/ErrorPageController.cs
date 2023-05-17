48c0725d60d0e0017d0a4ad9e19dd07c87786f7e
Sena <Sena@LAPTOP-KB17KV9I>
2023-05-17 17:01:33 +0300

    error page

---
+++ HotelCoreProject/Areas/Admin/Controllers/ErrorPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelCoreProject.Areas.Admin.Controllers
+{
+    public class ErrorPageController : Controller
+    {
+        public IActionResult Error404(int code)
+        {
+            return View();
+        }
+    }
+}
